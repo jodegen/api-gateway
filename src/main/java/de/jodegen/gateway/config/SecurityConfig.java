393cbec6b2be821c9538a874e1697669b602e68a
jodegen <jonasdegen01@gmail.com>
2025-05-16 14:13:35 +0200

    cors configuration

---
+++ src/main/java/de/jodegen/gateway/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package de.jodegen.gateway.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.web.cors.*;
+
+import java.util.*;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        corsConfiguration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+}
